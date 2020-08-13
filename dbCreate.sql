8cabdd35f06e8abd8065d5eb04dc01eccf2be89c
WistyDagel <chrisdazley@gmail.com>
2020-08-12 18:28:13 -0600

    Altered the SQL to match our database

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,6 +1,8 @@
-﻿use  MyTestDb;
+﻿CREATE DATABASE IF NOT EXISTS BeContentDB; 
 
-create table if not exists MyUsers(
+use  BeContentDB;
+
+create table if not exists Users(
  id int not null AUTO_INCREMENT PRIMARY KEY,
  First_Name varchar(25) Not null,
  Last_Name varchar(25) Not null,
@@ -11,79 +13,75 @@ create table if not exists MyUsers(
 );
 
 
-create table if not exists MyWebDocs(
+create table if not exists WebElements(
  id int not null AUTO_INCREMENT PRIMARY KEY,
  Title varchar(25) Not null,
 -- Category varchar(25),
- Header1 varchar(25),
- Text1 varchar(225),
+ Header varchar(25),
+ SubText varchar(225),
  ParentPage int DEFAULT 0,
  SortOrder int DEFAULT 2,
  isActive int
 );
 
 -- Sample data
-INSERT INTO MyUsers ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
-VALUES    (1, 'uFirstName', 'uLastName', 'myuser', 'a', 0, 1)
+INSERT INTO Users ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
+VALUES    (1, 'Chris', 'Dazley', 'cdazley', 'c', 0, 1)
 ON DUPLICATE KEY UPDATE
-First_Name = 'uFirstName', Last_Name = 'uLastName', UserId = 'myuser', Pswd = 'a', isAdmin = 0, isActive = 1;
+First_Name = 'Chris', Last_Name = 'Dazley', UserId = 'cdazley', Pswd = 'c', isAdmin = 0, isActive = 1;
 
-INSERT INTO MyUsers ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
-VALUES    (2, 'aFirstName', 'aLastName', 'myadmin', 'a', 1, 1)
+INSERT INTO Users ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
+VALUES    (2, 'Dery', 'Germann', 'dgermann', 'd', 0, 1)
 ON DUPLICATE KEY UPDATE
-First_Name = 'aFirstName', Last_Name = 'aLastName', UserId = 'myadmin', Pswd = 'a', isAdmin = 1, isActive = 1;
+First_Name = 'Dery', Last_Name = 'Germann', UserId = 'dgermann', Pswd = 'd', isAdmin = 0, isActive = 1;
 
-INSERT INTO MyUsers ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
-VALUES    (2, 'aFirstName', 'aLastName', 'myadmin', 'a', 1, 1)
+INSERT INTO Users ( id, First_Name, Last_Name, UserId, Pswd, isAdmin, isActive)
+VALUES    (3, 'Jeff', 'Rehm', 'jrehm', 'j', 1, 1)
 ON DUPLICATE KEY UPDATE
-First_Name = 'aFirstName', Last_Name = 'aLastName', UserId = 'myadmin', Pswd = 'a', isAdmin = 1, isActive = 1;
+First_Name = 'Jeff', Last_Name = 'Rehm', UserId = 'jrehm', Pswd = 'j', isAdmin = 1, isActive = 1;
 
 -- --------------------------------------
 -- Main links/pages
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, SortOrder, isActive)
-VALUES    (1, 'Home', 'Header number 1', 'My text, asfaf af af af a sag asf saf', 0, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (1, 'Home', 'Header number 1', 'This is my Header 1', 0, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Home', Header1 = 'Header number 1', Text1 = 'My text, asfaf af af af a sag asf saf', SortOrder = 0, isActive = 1;
+Title = 'Home', Header = 'Header number 1', SubText = 'This is my Header 1', SortOrder = 0, isActive = 1;
 
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, SortOrder, isActive)
-VALUES    (2, 'Something', 'Header number 2', 'My text, asfaf af af af a sag asf saf', 2, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (2, 'Something', 'Header number 2', 'This is my Header 2', 2, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'something', Header1 = 'Header number 2', Text1 = 'My text, asfaf af af af a sag asf saf', SortOrder = 2, isActive = 1;
+Title = 'something', Header = 'Header number 2', SubText = 'This is my Header 2', SortOrder = 2, isActive = 1;
 
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, SortOrder, isActive)
-VALUES    (3, 'About', 'Header number 3', 'My text, asfaf af af af a sag asf saf', 3, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (3, 'About', 'Header number 3', 'This is my Header 3', 3, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'About', Header1 = 'Header number 3', Text1 = 'My text, asfaf af af af a sag asf saf', SortOrder = 3, isActive = 1;
+Title = 'About', Header = 'Header number 3', SubText = 'This is my Header 3', SortOrder = 3, isActive = 1;
 
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, SortOrder, isActive)
-VALUES    (4, 'Contact Us', 'Header number 4', 'My text, asfaf af af af a sag asf saf', 4, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (4, 'Contact Us', 'Header number 4', 'This is my Header 4', 4, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Contact Us', Header1 = 'Header number 4', Text1 = 'My text, asfaf af af af a sag asf saf', SortOrder = 4, isActive = 1;
+Title = 'Contact Us', Header = 'Header number 4', SubText = 'This is my Header 4', SortOrder = 4, isActive = 1;
 
 -- ---------------------
 -- Sub pages
 -- Note Parent Id points to the record with id=1
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, ParentPage, SortOrder, isActive)
-VALUES    (5, 'Home 1', 'Sub Header number 1', 'My text, asfaf af af af a sag asf saf', 1, 3, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (5, 'Home 1', 'Sub Header number 1', 'This is my Sub Header 1 for ID 1', 1, 3, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Home 1', Header1 = 'Sub Header number 1', Text1 = 'My text, asfaf af af af a sag asf saf', ParentPage = 1, SortOrder = 3, isActive = 1;
+Title = 'Home 1', Header = 'Sub Header number 1', SubText = 'This is my Sub Header 1 for ID 1', ParentPage = 1, SortOrder = 3, isActive = 1;
 
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, ParentPage, SortOrder, isActive)
-VALUES    (6, 'Home 2', 'Sub Header number 2', 'My text, asfaf af af af a sag asf saf', 1, 4, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (6, 'Home 2', 'Sub Header number 2', 'This is my Sub Header 2 for ID 2', 1, 4, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Home 2', Header1 = 'Sub Header number 2', Text1 = 'My text, asfaf af af af a sag asf saf', ParentPage = 1, SortOrder = 4, isActive = 1;
+Title = 'Home 2', Header = 'Sub Header number 2', SubText = 'This is my Sub Header 2 for ID 2', ParentPage = 1, SortOrder = 4, isActive = 1;
 
 -- Note Parent Id points to the record with id=2
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, ParentPage, SortOrder, isActive)
-VALUES    (7, 'Something 1', 'Sub Header number 1', 'My text, asfaf af af af a sag asf saf', 2, 3, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (7, 'Something 1', 'Sub Header number 1', 'This is my Sub Header 1', 2, 3, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Something 1', Header1 = 'Sub Header number 1', Text1 = 'My text, asfaf af af af a sag asf saf', ParentPage = 2, SortOrder = 3, isActive = 1;
+Title = 'Something 1', Header = 'Sub Header number 1', SubText = 'This is my Sub Header 1 for ID 2', ParentPage = 2, SortOrder = 3, isActive = 1;
 
-INSERT INTO MyWebDocs ( id, Title, Header1, Text1, ParentPage, SortOrder, isActive)
-VALUES    (8, 'Something 2', 'Sub Header number 2', 'My text, asfaf af af af a sag asf saf', 2, 4, 1)
+INSERT INTO WebElements ( id, Title, Header, SubText, SortOrder, isActive)
+VALUES    (8, 'Something 2', 'Sub Header number 2', 'This is my Sub Header 2', 2, 4, 1)
 ON DUPLICATE KEY UPDATE
-Title = 'Something 2', Header1 = 'Sub Header number 2', Text1 = 'My text, asfaf af af af a sag asf saf', ParentPage = 2, SortOrder = 4, isActive = 1;
-
-
-
-
+Title = 'Something 2', Header = 'Sub Header number 2', SubText = 'This is my Sub Header 2 for ID 2', ParentPage = 2, SortOrder = 4, isActive = 1;
