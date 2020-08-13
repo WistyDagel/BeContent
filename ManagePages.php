e7c52f2273a8b6babffe68415487fac8f0e246d5
WistyDagel <chrisdazley@gmail.com>
2020-08-13 12:35:31 -0600

    Centered the manage pages and gave it a title

--- ManagePages.php
+++ ManagePages.php
@@ -32,47 +32,51 @@ if (isset($_POST)) {
 
 ?>
 
-<!-- Beginning of pages table and table headers -->
-<table>
-<tr>
-    <th>Id</th>
-    <th>Title</th>
-    <th>Header</th>
-    <th>SubText</th>
-    <th>ParentPage</th>
-    <th>SortOrder</th>
-    <th>IsActive</th>
-    <th></th>
-    <th></th>
-    <th></th>
-</tr>
+<h2>Current Pages</h2>
 
-<?php
-
-// Generate table row for each page
-while ($page = mysqli_fetch_array($pages)) {
-    // One form per table row
-    echo '<form method="post"><tr>';
-    // Hidden input to post table id
-    echo '<input type="hidden" name="id" value="' . $page['id'] . '" />';
-    echo '<td>' . $page['id'] . '</td>';
-    echo '<td>' . $page['Title'] . '</td>';
-    echo '<td>' . $page['Header'] . '</td>';
-    echo '<td>' . $page['SubText'] . '</td>';
-    echo '<td>' . $page['ParentPage'] . '</td>';
-    echo '<td>' . $page['SortOrder'] . '</td>';
-    echo '<td>' . $page['isActive'] . '</td>';
-    // Submit buttons to post form
-    echo '<td><input type="submit" name="edit" value="Edit" /></td>';
-    echo '<td><input type="submit" name="deactivate" value="Deactivate" /></td>';
-    echo '<td><input type="submit" name="activate" value="Activate" /></td>';
-    echo '</tr></form>';
-}
-
-?>
-
-<!-- Close tags -->
-</table>
+<div class="flexContainer">
+    <!-- Beginning of pages table and table headers -->
+    <table>
+    <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Header</th>
+        <th>SubText</th>
+        <th>ParentPage</th>
+        <th>SortOrder</th>
+        <th>IsActive</th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    
+    <?php
+    
+    // Generate table row for each page
+    while ($page = mysqli_fetch_array($pages)) {
+        // One form per table row
+        echo '<form method="post"><tr>';
+        // Hidden input to post table id
+        echo '<input type="hidden" name="id" value="' . $page['id'] . '" />';
+        echo '<td>' . $page['id'] . '</td>';
+        echo '<td>' . $page['Title'] . '</td>';
+        echo '<td>' . $page['Header'] . '</td>';
+        echo '<td>' . $page['SubText'] . '</td>';
+        echo '<td>' . $page['ParentPage'] . '</td>';
+        echo '<td>' . $page['SortOrder'] . '</td>';
+        echo '<td>' . $page['isActive'] . '</td>';
+        // Submit buttons to post form
+        echo '<td><input type="submit" name="edit" value="Edit" /></td>';
+        echo '<td><input type="submit" name="deactivate" value="Deactivate" /></td>';
+        echo '<td><input type="submit" name="activate" value="Activate" /></td>';
+        echo '</tr></form>';
+    }
+    
+    ?>
+    
+    <!-- Close tags -->
+    </table>
+</div>
 
 <?php
 include_once "MyHeader.php";
